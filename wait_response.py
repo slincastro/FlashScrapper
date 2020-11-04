19497f2a33319abb8a4cf7a18050f878a8127b46
Slin Castro <scastro@thoughtworks.com>
2020-11-03 19:35:10 -0500

    Update get speed configuration

--- wait_response.py
+++ wait_response.py
@@ -33,7 +33,7 @@ def write_speed():
 def in_minutes(minutes):
     return minutes * 60
 
-for i in range(10):
-    time.sleep(in_minutes(1))
+for i in range(200):
+    time.sleep(in_minutes(5))
     write_speed()
 
